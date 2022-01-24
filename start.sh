b7f5b64961349d60e7f9890c01f1944ef78dcdd6
【★༒✞︎Drak-ℓєɠεɳ∂-Pro】 <98252847+DARK-LEGEND-PRO@users.noreply.github.com>
2022-01-24 21:36:01 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo """
+                               
+"""
+rm -rf python_support_zone
+git clone https://github.com/LEGEND-LX/python_support_zone
+cd python_support_zone
+python3 -m userbot
