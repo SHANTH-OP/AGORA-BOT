06c3b229fb758248d8e0e79d7b1dfd88af97dd2a
PROFESSOR AGORA <98295372+ProfessorAgora@users.noreply.github.com>
2022-01-26 13:43:19 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,13 +8,13 @@ from var import Var
 from userbot.Config import Config
 from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
 from userbot.utils import load_module
-from userbot import LOAD_PLUG, LOGS, PYTHONversion
+from userbot import LOAD_PLUG, LOGS, AGORAversion
 from pathlib import Path
 import asyncio
 import glob
 import telethon.utils
 l2= Config.SUDO_COMMAND_HAND_LER
-PYTHON_PIC = Config.ALIVE_PIC or "https://telegra.ph/file/75e1eda1498620f0030ea.jpg"
+AGORA_PIC = Config.ALIVE_PIC or "https://te.legra.ph/file/78a7494a15602979a9241.mp4"
 l1 = Config.COMMAND_HAND_LER
 
 
@@ -67,14 +67,14 @@ async def assistant():
         shortname = path1.stem
         start_assistant(shortname.replace(".py", ""))
 
-        extra_repo = "https://github.com/LEGEND-LX/PYTHONUSERBOT"
+        extra_repo = "https://github.com/SHANTH-OP/AGORA-BOT"
         try:
             os.system(f"git clone {extra_repo}")  
         except BaseException:
             pass
         import glob
         LOGS.info("Loading Addons")
-        path = "PYTHONUSERBOT/*.py"
+        path = "AGORA-BOT/*.py"
         files = glob.glob(path)
         for name in files:
             with open(name) as ex:
@@ -83,23 +83,23 @@ async def assistant():
                 try:
                     load_addons(shortname.replace(".py", ""))
                     if not shortname.startswith("__") or shortname.startswith("_"):
-                        LOGS.info(f"[PYTHON-BOT 9.0.8] - Addons -  Installed - {shortname}")
+                        LOGS.info(f"[AGORA-USERBOT] - Addons -  Installed - {shortname}")
                 except Exception as e:
-                    LOGS.warning(f"[PYTHON-BOT 9.0.8] - Addons - ERROR - {shortname}")
+                    LOGS.warning(f"[AGORA-USERBOT] - Addons - ERROR - {shortname}")
                     LOGS.warning(str(e))
     else:
         print("Addons Not Loading")
 """
 bot.loop.run_until_complete(module())
 
-print(f"""『🔱PYTHON-USER-BOT🔱』➙𖤍࿐ IS ON!!! PYTHON VERSION :- {PYTHONversion}
-TYPE :- " .gpromote @Legendl_Mr_Hacker " OR .python OR .ping CHECK IF I'M ON!
-╔════❰PYTHONBOT❱═❍⊱❁۪۪
-║┣⪼ OWNER - LEGEND-LX
-║┣⪼{PYTHON_PIC}
-║┣⪼ CREATOR -@Legendl_Mr_Hacker
+print(f"""『☘️ AGORA-USERBOT ☘️』➙𖤍࿐ IS ON!!! AGORA VERSION :- {AGORAversion}
+TYPE :- " .gpromote @AGORASWAMY_PROFESSOR " OR .agora OR .ping CHECK IF I'M ON!
+╔════❰AGORABOT❱═❍⊱❁۪۪
+║┣⪼ OWNER - PROFESSOR AGORA
+║┣⪼{AGORA_PIC}
+║┣⪼ CREATOR -@AGORASWAMY_PROFESSOR
 ║┣⪼ TELETHON - 1.2.0
-║┣⪼ ✨ 『🔱PYTHON-USER-BOT 🔱』𝐔𝐬𝐞𝐫𝐛𝐨𝐭✨
+║┣⪼ ✨ 『☘️ AGORA-USERBOT ☘️』𝐔𝐬𝐞𝐫𝐛𝐨𝐭✨
 ║╰━━━━━━━━━━━━━━━➣
 ╚══════════════════❍⊱""")
 
@@ -110,25 +110,25 @@ async def python_is_on():
         if Config.LOGGER_ID != 0:
             await bot.send_file(
                 Config.LOGGER_ID,
-                PYTHON_PIC,
-                caption=f"#START \n\nDeployed PYTHONBOT Successfully\n\n**PYTHONBOT- {PYTHONversion}**\n\nType `{l1}python` or `{l1}pyalive` to check! \n\nJoin [PythonBot Channel](t.me/Python_Updata) for Updates & [PythonBot Chat](t.me/Python_Userbot_Support) for any query regarding PythonBot",
+                PYTHON_PIC.
+                caption=f"#START \n\nDeployed AGORA-USERBOT Successfully\n\n**AGORA-USERBOT- {AGORAversion}**\n\nType `{l1}agora` or `{l1}alive` to check! \n\nJoin [AgoraBot Channel](t.me/Agorabot_info) for Updates & [AgoraBot Chat](t.me/Agora_Userbot) for any query regarding AgoraBot",
             )
     except Exception as e:
         print(str(e))
 
-# Join PythonBot Channel after deploying 🤐😅
+# Join AgoraBot Channel after deploying 🤐😅
     try:
-        await bot(JoinChannelRequest("@Python_Updata"))
+        await bot(JoinChannelRequest("@Agorabot_info"))
     except BaseException:
         pass
 
     try:
-        await bot(JoinChannelRequest("@Python_Userbot_Support"))
+        await bot(JoinChannelRequest("@agora_userbot"))
     except BaseException:
          pass
 
 
-bot.loop.create_task(python_is_on())
+bot.loop.create_task(agora_is_on())
 
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
