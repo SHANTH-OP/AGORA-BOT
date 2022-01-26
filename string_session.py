575c7e8587b7ed104ef8a4fb2af60d857a9ac720
PROFESSOR AGORA <98295372+ProfessorAgora@users.noreply.github.com>
2022-01-26 12:05:16 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,10 +1,10 @@
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-PYTHONBOT = """
+AGORA-USERBOT = """
 """
-print(PYTHONBOT)
-print("""String Generator. ==> PYTHONBot. Get Your Api Id & Api Hash From my.telegram.org and fill accordingly.
+print(AGORA-USERBOT)
+print("""String Generator. ==> AGORABot. Get Your Api Id & Api Hash From my.telegram.org and fill accordingly.
 ╭━━━┳╮╱╱╭┳━━━━┳╮╱╭┳━━━┳━╮╱╭╮
 ┃╭━╮┃╰╮╭╯┃╭╮╭╮┃┃╱┃┃╭━╮┃┃╰╮┃┃
 ┃╰━╯┣╮╰╯╭┻╯┃┃╰┫╰━╯┃┃╱┃┃╭╮╰╯┃
@@ -24,9 +24,9 @@ with TelegramClient(StringSession(), APP_ID, API_HASH) as PYTHONBOT:
     print("")
     print("This is your STRING_SESSION. Please Keep It safe.")
     print("")
-    print(PYTHONBOT.session.save())
+    print(AGORA-USERBOT.session.save())
     print("")
     print("You can Get Your String Session In Saved Message of Your Telegram Account. Remember To Make New String Session Whenever You Terminate Sessions.")
-    omk =PYTHONBOT.send_message("me", f"`{PYTHONBOT.session.save()}`")
-    omk.reply("The above is the `PYTHON_STRING` #POWERFUL [PYTHONBOT](https://t.me/Python_Userbot_Support)"
+    omk =AGORA-USERBOT.send_message("me", f"`{AGORA-USERBOT.session.save()}`")
+    omk.reply("The above is the `AGORA_STRING` #POWERFUL [AGORA-USERBOT](https://t.me/AGORA_SPAM_OFFICIAL)"
 		)
